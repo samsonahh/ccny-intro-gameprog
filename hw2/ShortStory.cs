8e1d0d6e40e248c1b6ffb2c78e12f44da8a53a9c
Samson Wu <samsonwu05@gmail.com>
2024-02-21 12:17:43 -0500

    hw2 init

---
+++ hw2/ShortStory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortStory : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("yadayadaya");
+    }
+}
